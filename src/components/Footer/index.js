25bb05ee30d72ad1ff1fa7de62c23d108833758e
reneecosmic <renee.hickman@outlook.com>
2019-04-26 23:43:05 -0500

    components and styling

---
+++ src/components/Footer/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './style.css';
+
+
+
+// const styles = ({
+//   height: {
+//     height: "25px",
+//     width: "25px",
+//   },
+// });
+
+
+const Footer = () => {
+    return (
+      <div className="footer">
+        <button type="button" className="btn btn-secondary btn-sm" href="https://github.com/reneehickman/ClickyGame/"><i className="fa fa-code" aria-hidden="true"></i> Github Repo</button>
+      </div>
+    ); 
+  }
+
+export default Footer;
